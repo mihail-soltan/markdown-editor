03ede5846c6b062a1975ce7231541626f944522b
mihail-soltan <mihail.soltan@protonmail.ch>
2023-03-24 18:24:09 +0200

    fix highligh issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ng-markdown-editor';
+
+  code: string = `\`\`\`
+  typescript const dog = () => {console.log("woof")}
+  
+  \`\`\`
+
+  `;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MarkdownModule, MarkdownService } from 'ngx-markdown';
 import { AppComponent } from './app.component';
-
+import "prismjs/prism";
+import "prismjs/plugins/line-numbers/prism-line-numbers.js"
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MarkdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
