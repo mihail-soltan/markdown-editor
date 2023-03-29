a47139d17e33085e39f44cb8eaccf6a009be3ac0
mihail-soltan <mihail.soltan@protonmail.ch>
2023-03-29 23:55:29 +0300

    add darkmode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from './services/shared.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  lightmode: boolean = false;
+
+  constructor(private shared: SharedService){}
   title = 'ng-markdown-editor';
 
   code: string = `\`\`\`typescript
@@ -17,4 +20,9 @@ export class AppComponent {
   blockQuote: string = `
     > Testing block quote
   `
+  ngOnInit(){
+    this.shared.lightmode.subscribe((theme)=>{
+      this.lightmode = theme;
+    })
+  }
 }
--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/services/shared.service';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss']
 })
-export class SidenavComponent {
+export class SidenavComponent implements OnInit{
 
+  lightmode: boolean = false;
+
+  constructor(private shared: SharedService){}
+  
+  ngOnInit(){
+    this.shared.lightmode.subscribe((theme) =>{
+      this.lightmode = theme;
+    })
+  }
+
+  toggleTheme(){
+    this.shared.toggleTheme()
+    console.log(this.lightmode)
+  }
 }
